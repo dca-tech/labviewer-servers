0146f8c6d505163e8b4cd7404e01b236c18c2df6
Jhon Carlos Solis Ochoa <jcsolis78@gmail.com>
2022-09-28 15:09:29 -0500

    Despliegue de componentes de seguridad de labviewer

---
+++ labviewer-servicio-gateway-server/src/test/java/com/dca/labviewer/app/gateway/LabviewerGatewayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dca.labviewer.app.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabviewerGatewayServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ labviewer-servicio-oauth/src/test/java/com/labviewer/app/oauth/LabviewerServicioOauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.labviewer.app.oauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabviewerServicioOauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
